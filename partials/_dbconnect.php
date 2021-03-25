712f90ae4e0f816c38a0d09dbd738b906b08d5c7
Pranshu <pranshup23@gmail.com>
2021-03-25 17:38:25 +0530

    First commit

---
+++ partials/_dbconnect.php
@@ -0,0 +1,11 @@
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "ecomm";
+
+$conn = mysqli_connect($server, $username, $password, $database);
+
+
+?>
---
+++ partials/_footer.php
@@ -0,0 +1,4 @@
+
+<div class="fixed-bottom container-fluid bg-danger text-light">
+<p class="text-center mb-0">© Copyright 2020-2021 | E-Community - Coding Forum | All right reserved</p>
+</div>
